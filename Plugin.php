7653285fed2b13a7d30d861383bfcf2340780fc9
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-07-30 15:34:23 +0200

    Added CreativeWork component

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,7 @@
 <?php namespace Magiczne\JsonLd;
 
 use Magiczne\JsonLd\Components\Action;
+use Magiczne\JsonLd\Components\CreativeWork;
 use Magiczne\JsonLd\Components\MedicalEntity;
 use Magiczne\JsonLd\Components\Organization;
 use Magiczne\JsonLd\Components\Person;
@@ -19,6 +20,7 @@ class Plugin extends PluginBase
 
             // Level 1
             Action::class => 'Action',
+            CreativeWork::class => 'CreativeWork',
             MedicalEntity::class => 'MedicalEntity',
             Organization::class => 'Organization',
             Person::class => 'Person',
---
+++ components/CreativeWork.php
@@ -0,0 +1,98 @@
+<?php namespace Magiczne\JsonLd\Components;
+
+class CreativeWork extends Thing
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'CreativeWork',
+            'description' => 'https://schema.org/CreativeWork'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return array_merge(parent::defineProperties(), $this->generateProperties([
+            'about',
+            'accessMode',
+            'accessModeSufficient',
+            'accessibilityAPI',
+            'accessibilityControl',
+            'accessibilityFeature',
+            'accessibilityHazard',
+            'accessibilitySummary',
+            'accountablePerson',
+            'aggregateRating',
+            'alternativeHeadline',
+            'associatedMedia',
+            'audience',
+            'audio',
+            'author',
+            'award',
+            'character',
+            'citation',
+            'comment',
+            'commentCount',
+            'contentLocation',
+            'contentRating',
+            'contributor',
+            'copyrightHolder',
+            'copyrightYear',
+            'creator',
+            'dateCreated',
+            'dateModified',
+            'datePublished',
+            'discussionUrl',
+            'editor',
+            'educationalAlignment',
+            'educationalUse',
+            'encoding',
+            'encodingFormat',
+            'exampleOfWork',
+            'expires',
+            'funder',
+            'genre',
+            'hasPart',
+            'headline',
+            'inLanguage',
+            'interactionStatistic',
+            'interactivityType',
+            'isAccessibleForFree' => $this->dropdownBoolean,
+            'isBasedOn',
+            'isFamilyFriendly' => $this->dropdownBoolean,
+            'isPartOf',
+            'keywords',
+            'learningResourceType',
+            'license',
+            'locationCreated',
+            'mainEntity',
+            'material',
+            'mentions',
+            'offers',
+            'position',
+            'producer',
+            'provider',
+            'publication',
+            'publisher',
+            'publishingPrinciples',
+            'recordedAt',
+            'releasedEvent',
+            'review',
+            'schemaVersion',
+            'sourceOrganization',
+            'spatial',
+            'spatialCoverage',
+            'sponsor',
+            'temporal',
+            'temporalCoverage',
+            'text',
+            'thumbnailUrl',
+            'timeRequired',
+            'translator',
+            'typicalAgeRange',
+            'version',
+            'video',
+            'workExample'
+        ], 'CreativeWork'));
+    }
+}
