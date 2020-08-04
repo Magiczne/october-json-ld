3fb3b9f36920d63f167466aee50ad075eaa2f426
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2020-08-04 19:30:25 +0200

    Added static field which holds Organization raw property names

--- components/level1/Organization.php
+++ components/level1/Organization.php
@@ -4,6 +4,58 @@ use Magiczne\JsonLd\Components\Thing;
 
 class Organization extends Thing
 {
+    /**
+     * Properties existing on Organization schema.
+     *
+     * Since for example EducationalOrganization is and Organization and a Place as well,
+     * there is a need to store it as a static property in order for other classes to use it.
+     */
+    public static $rawProperties = [
+        'address',
+        'aggregateRating',
+        'alumni',
+        'areaServed',
+        'award',
+        'brand',
+        'contactPoint',
+        'department',
+        'dissolutionDate',
+        'duns',
+        'email',
+        'employee',
+        'event',
+        'faxNumber',
+        'founder',
+        'foundingDate',
+        'foundingLocation',
+        'funder',
+        'globalLocationNumber',
+        'hasOfferCatalog',
+        'hasPOS',
+        'interactionStatistic',
+        'isicV4',
+        'legalName',
+        'leiCode',
+        'location',
+        'logo',
+        'makesOffer',
+        'member',
+        'memberOf',
+        'naics',
+        'numberOfEmployees',
+        'owns',
+        'parentOrganization',
+        'publishingPrinciples',
+        'review',
+        'seeks',
+        'slogan',
+        'sponsor',
+        'subOrganization',
+        'taxID',
+        'telephone',
+        'vatID'
+    ];
+
     public function componentDetails()
     {
         return [
@@ -14,50 +66,8 @@ class Organization extends Thing
 
     public function defineProperties()
     {
-        return array_merge(parent::defineProperties(), $this->generateProperties([
-            'address',
-            'aggregateRating',
-            'alumni',
-            'areaServed',
-            'award',
-            'brand',
-            'contactPoint',
-            'department',
-            'dissolutionDate',
-            'duns',
-            'email',
-            'employee',
-            'event',
-            'faxNumber',
-            'founder',
-            'foundingDate',
-            'foundingLocation',
-            'funder',
-            'globalLocationNumber',
-            'hasOfferCatalog',
-            'hasPOS',
-            'interactionStatistic',
-            'isicV4',
-            'legalName',
-            'leiCode',
-            'location',
-            'logo',
-            'makesOffer',
-            'member',
-            'memberOf',
-            'naics',
-            'numberOfEmployees',
-            'owns',
-            'parentOrganization',
-            'publishingPrinciples',
-            'review',
-            'seeks',
-            'slogan',
-            'sponsor',
-            'subOrganization',
-            'taxID',
-            'telephone',
-            'vatID'
-        ], 'Organization'));
+        $generatedProperties = $this->generateProperties(self::$rawProperties, 'Organization');
+
+        return array_merge(parent::defineProperties(), $generatedProperties);
     }
 }
